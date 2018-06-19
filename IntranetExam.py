f16552f636e72d226d932697be4fbc3ec2a44799
Mathana Kumar S <36666781+capturemathan@users.noreply.github.com>
2018-06-19 21:21:52 +0530

    Add files via upload

---
+++ IntranetExam.py
@@ -0,0 +1,96 @@
+import webbrowser
+import time
+import random
+import requests
+from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+from time import sleep
+
+print("Welcome to Qp Bank !")
+sleep(2)
+print("Crafted with love by Mathan.S")
+sleep(2)
+a=["M234x","Ad34T","Fr45C","J234r","PKa67"]
+z=random.randint(0,4)
+print(a[z])
+captcha=input("Enter the captcha")
+while(captcha!=a[z]):
+	print("Enter the correct captcha..")
+	a=["M234x","Ad34T","Fr45C","J234r","PKa67"]
+	z=random.randint(0,4)
+	print(a[z])
+	captcha=input("Enter the captcha")
+
+url="http://dspace.amritanet.edu:8080/xmlui/handle/123456789/150"
+page=requests.get(url)
+soup=BeautifulSoup(page.content,'html.parser')
+div=soup.div
+main_div=soup.find(id="aspect_artifactbrowser_CommunityViewer_div_community-view")
+t=PrettyTable(["S.No","Attribute"])
+main_list_item=main_div.ul
+items=main_list_item.findAll("li")
+for i in range(len(items)):
+	t.add_row([i+1,items[i].a.text.strip()])
+print(t)
+ch=int(input("Enter your Semester"))
+url="http://dspace.amritanet.edu:8080"
+url+=items[ch-1].a["href"]
+
+print("Give me just a minute...")
+
+sec_page=requests.get(url)
+sec_soup=BeautifulSoup(sec_page.content,'html.parser')
+u=sec_soup.findAll("ul")
+
+
+if ch<=6:
+	sec_li=u[3].findAll("li")
+else:
+	sec_li=u[2].findAll("li")
+
+
+p=PrettyTable(["S.No","Title"])
+for j in range(len(sec_li)):
+	p.add_row([j+1,sec_li[j].a.text.strip()])
+print(p)
+
+
+ch3=int(input("Enter your choice"))
+url="http://dspace.amritanet.edu:8080/"
+url+=sec_li[ch3-1].a["href"]
+third_page=requests.get(url)
+third_soup=BeautifulSoup(third_page.content,'html.parser')
+u3_div=third_soup.findAll("div",class_="ds-static-div secondary recent-submission")
+third_div=u3_div[0].a.text.strip()
+third_li=u3_div[0].findAll("li")
+
+m=PrettyTable(["S.No","Title"])
+m.add_row([1,third_div])
+print(m)
+
+ch4=int(input("Enter your choice"))
+url="http://dspace.amritanet.edu:8080/"
+url+=third_li[ch4-1].a["href"]
+fourth_page=requests.get(url)
+fourth_soup=BeautifulSoup(fourth_page.content,'html.parser')
+u4_div=fourth_soup.findAll("div",class_="file-metadata")
+
+
+v=PrettyTable(["S.No","Subjects"])
+u4_temp=[]
+mod_u4_temp=[]
+for i in range(len(u4_div)):
+	u4_temp.append((u4_div[i].findAll("span")[1].text))
+for j in range(len(u4_temp)):
+	mod_u4_temp=u4_temp[j].split(",")
+	v.add_row([j+1,mod_u4_temp[0]])
+
+print(v)
+
+last_ch=int(input("Enter your choice"))
+last_div=fourth_soup.findAll("div",class_="file-link")
+url_last="http://dspace.amritanet.edu:8080"
+url_last+=last_div[last_ch-1].a["href"]
+
+print("All the very best for your exams ")
+webbrowser.open_new(url_last)
